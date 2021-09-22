6ea5e46cc696b3be7937a060d418a6f018644331
Greshan Warnakulasuriya <greshanw@hotmail.com>
2021-09-22 10:40:10 +0530

    Add cart activity

--- app/src/main/java/com/example/project_mad/My_Cart_Fragment.java
+++ app/src/main/java/com/example/project_mad/My_Cart_Fragment.java
@@ -1,64 +1,99 @@
 package com.example.project_mad;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link My_Cart_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.project_mad.adapters.CartAdapter;
+import com.example.project_mad.models.CartModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class My_Cart_Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FirebaseFirestore db;
+    FirebaseAuth auth;
+    float total = 0;
+    TextView tot;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView recyclerView;
+    CartAdapter cartAdapter;
+    List<CartModel> cartModelList;
+
 
     public My_Cart_Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment My_Cart_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static My_Cart_Fragment newInstance(String param1, String param2) {
-        My_Cart_Fragment fragment = new My_Cart_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my__cart_, container, false);
+        View root =inflater.inflate(R.layout.fragment_my__cart_, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        recyclerView = root.findViewById(R.id.recyclerview);
+
+        LocalBroadcastManager.getInstance(getActivity())
+                .registerReceiver(mMessageReceiver, new IntentFilter("cartTotal"));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView .VERTICAL,false));
+
+        cartModelList = new ArrayList<>();
+        cartAdapter = new CartAdapter(getActivity(), cartModelList);
+        recyclerView.setAdapter(cartAdapter);
+
+        db.collection("AddToCart").document(auth.getCurrentUser().getUid())
+                .collection("CurrentUser").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult().getDocuments()) {
+                                String documentId = document.getId();
+
+                                CartModel cartModel = document.toObject(CartModel.class);
+                                cartModel.setDocumentId(documentId);
+
+                                cartModelList.add(cartModel);
+                                cartAdapter.notifyDataSetChanged();
+
+                            }
+                        } else {
+                            Toast.makeText(getActivity(),"Error"+task.getException(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        return  root;
     }
+
+    public BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int counttot = intent.getIntExtra("total", 0);
+            tot.setText(String.valueOf(counttot));
+        }
+    };
 }
--- app/src/main/java/com/example/project_mad/ProductActivity.java
+++ app/src/main/java/com/example/project_mad/ProductActivity.java
@@ -157,7 +157,7 @@ public class ProductActivity extends AppCompatActivity {
         cartMap.put("currentDate", saveCurrentDate);
         cartMap.put("currentTime", saveCurrentTime);
         cartMap.put("totalQuantity", quantity.getText().toString());
-        cartMap.put("totalPrice", totalPrice);
+        cartMap.put("productImg", viewAllModel.getImg_url());
 
         firestore.collection("AddToCart").document(auth.getCurrentUser().getUid())
                 .collection("CurrentUser").add(cartMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
